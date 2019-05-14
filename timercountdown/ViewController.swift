a69be5bec23e8432c082337a4eeb7441bb3c67a6
Abdelilah HEDDAR <aheddar@fashfish.io>
2019-05-14 14:33:16 +0200

    Initial Commit

---
+++ timercountdown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  timercountdown
+//
+//  Created by Abdelilah Heddar on 14/05/2019.
+//  Copyright © 2019 Abdelilah Heddar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
